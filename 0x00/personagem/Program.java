6e0cdbffaee3dc0be048253feacb767f88e44a31
rsousa <rsousa@ciandt.com>
2022-08-17 12:20:29 -0300

    fix: pt/br locale added to pass in test

--- 0x00/personagem/Program.java
+++ 0x00/personagem/Program.java
@@ -1,5 +1,9 @@
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "br"));
+
         PersonagemFactory terraMediaPersonagemFactory = new WesterosPersonagemFactory();
         PersonagemFactory westerosPersonagemFactory = new TerraMediaPersonagemFactory();
 
