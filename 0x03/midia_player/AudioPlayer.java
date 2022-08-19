9515f15589b61ce2e4ccc917c56dcf0d1a765a56
rsousa <rsousa@ciandt.com>
2022-08-19 08:44:21 -0300

    fix: "\n" removed from reproduzir... methods

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -4,7 +4,7 @@ public class AudioPlayer implements MediaPlayer {
     @Override
     public void reproduzir(TipoMedia tipoMedia, String nomeArquivo) {
         if (tipoMedia == TipoMedia.MP3) {
-            System.out.printf("Reproduzindo MP3: %s\n", nomeArquivo);
+            System.out.printf("Reproduzindo MP3: %s", nomeArquivo);
         }
 
         mediaPlayerAdapter = new MediaPlayerAdapter(tipoMedia);
--- 0x03/midia_player/VideoMediaPlayer.java
+++ 0x03/midia_player/VideoMediaPlayer.java
@@ -1,11 +1,11 @@
 public class VideoMediaPlayer implements AdvancedMediaPlayer {
     @Override
     public void reproduzirVlc(String nomeArquivo) {
-        System.out.printf("Reproduzindo VLC: %s\n", nomeArquivo);
+        System.out.printf("Reproduzindo VLC: %s", nomeArquivo);
     }
 
     @Override
     public void reproduzirMp4(String nomeArquivo) {
-        System.out.printf("Reproduzindo MP4: %s\n", nomeArquivo);
+        System.out.printf("Reproduzindo MP4: %s", nomeArquivo);
     }
 }
